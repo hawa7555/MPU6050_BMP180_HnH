2dca75f6c1f081e7435dcc5084965cd030c1566d
harshalWadhwa <harshal16@hotmail.com>
2020-07-03 20:25:04 +0530

    Selection done,but with polling of receiver

--- MPU6050_HnH_final.ino
+++ MPU6050_HnH_final.ino
@@ -2,10 +2,13 @@
 #include "MPU6050_6Axis_MotionApps20.h"
 #include "Wire.h"
 #include <avr/wdt.h>
+#include <SoftwareSerial.h>
 
 #define g 9.80665f
 #define accelRange 16384.0
 #define gyroRange 65.5
+
+SoftwareSerial bluetooth(7, 8);  //rx-tx
 
 MPU6050 mpu;
 
@@ -28,19 +31,19 @@ float gyroX, gyroY, gyroZ;
 float accelX_off, accelY_off, accelZ_off;
 float gyroX_off, gyroY_off, gyroZ_off;
 
-char selection = '0';
+char selection;
+String str;
+char data[10];
 
 void setup() {
-  pinMode(7, OUTPUT);
-  
-  digitalWrite(7, HIGH);
-  delay(1000);
-  digitalWrite(7, LOW);
-    
   Wire.begin();
   //Wire.setClock(400000);
+  Serial.begin(115200);
+  Serial.println("Initializing I2C devices...");
   mpu.initialize();
   mpu.setFullScaleGyroRange(MPU6050_GYRO_FS_500);        //500dps, full range
+  Serial.println(mpu.testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
+  Serial.println(F("Initializing DMP..."));
   devStatus = mpu.dmpInitialize();
 
   mpu.setXGyroOffset(220);
@@ -49,6 +52,7 @@ void setup() {
   mpu.setZAccelOffset(1788);
 
   if (devStatus) {
+    Serial.println("DMP Init Failed");
     while (1);
   }
   mpu.CalibrateAccel(100);
@@ -56,6 +60,7 @@ void setup() {
   mpu.PrintActiveOffsets();
   mpu.setDMPEnabled(true);
   packetSize = mpu.dmpGetFIFOPacketSize();     //packetsize is 42
+  Serial.println(packetSize);
 
   gyroX_off = mpu.getXGyroOffset();
   gyroY_off = mpu.getYGyroOffset();
@@ -64,17 +69,21 @@ void setup() {
   accelY_off = mpu.getYAccelOffset();
   accelZ_off = mpu.getZAccelOffset();
 
+  bluetooth.begin(9600);
   wdt_disable();
   delay(3000);        //to avoid infinite resetting
-  selection = 0;      //initial value
+  selection = '0';   //initial value
 }
 
 void loop() {
+  if( bluetooth.available() > 0) {
+    selection = bluetooth.read();
+  }
   switch(selection) {
     case '0': getOrientation(); break;
     case '1': getAccelReadings(); break;
     case '2': getGyroReadings(); break;
-    case '3': sendString("97000"); break;
+    case '3': bluetooth.write("*9700"); delay(100); wdt_reset(); break;
   }
 }
 
@@ -87,7 +96,7 @@ void getOrientation() {
   else if (fifoCount % packetSize != 0) {
     return;
   }
-  wdt_enable(WDTO_4S);                 //watchDog timer for 4sec
+  wdt_enable(WDTO_8S);                 //watchDog timer for 4sec
   while (fifoCount < packetSize) fifoCount = mpu.getFIFOCount();    //wait for correct data length, very short duration
 
   while (fifoCount >= packetSize) {                    //to read latest packet
@@ -101,56 +110,38 @@ void getOrientation() {
   y = ypr[0] * 180 / M_PI;
   p = ypr[1] * 180 / M_PI;
   r = ypr[2] * 180 / M_PI;
+  //Serial.println(p);
+  str = "*" + String(p);
+  str.toCharArray(data, 10);
+  bluetooth.write(data);
   delay(100);
   wdt_reset();
 }
 
 void getAccelReadings() {
-  wdt_enable(WDTO_4S);               //watchDog timer for 4sec
+  wdt_enable(WDTO_8S);               //watchDog timer for 4sec
   mpu.getAcceleration(&ax, &ay, &az);
   accelX = (ax - accelX_off) / accelRange * g;
   accelY = (ay - accelY_off) / accelRange * g;
   accelZ = (az - accelZ_off) / accelRange * g;
+  //Serial.println(accelY);
+  str = "*" + String(accelZ);
+  str.toCharArray(data, 10);
+  bluetooth.write(data);
   delay(100);
   wdt_reset();
 }
 
 void getGyroReadings() {
-  wdt_enable(WDTO_4S);               //watchDog timer for 4sec
+  wdt_enable(WDTO_8S);               //watchDog timer for 4sec
   mpu.getRotation(&gx, &gy, &gz);
   gyroX = (gx - gyroX_off) / gyroRange;
   gyroY = (gy - gyroY_off) / gyroRange;
   gyroZ = (gz - gyroZ_off) / gyroRange;
+  //Serial.println(gyroZ);
+  str = "*" + String(gyroZ);
+  str.toCharArray(data, 10);
+  bluetooth.write(data);
   delay(100);
   wdt_reset();
-}
-
-void uartInit() {
-  //baud rate, 9600
-  UBRR0H = 0x00;
-  UBRR0L = 0x67;
-
-  UCSR0B |= (1 << 4) | (1 << 3); //receiver and transmitter enable
-  UCSR0C |= (1 << 2) | (1 << 1); //8-bit data size
-  UCSR0B |= (1 << RXCIE0);      //receiver interrupt enable
-  sei();               //global interrupt enable
-}
-
-void sendChar(char data) {
-  UDR0 = data;
-  while (!(UCSR0A & (1 << UDRE0)) ); //wait to send data
-}
-
-char receiveData() {
-  while (!(UCSR0A & (1 << 7)) ); //wait to receive data
-  char data = UDR0;
-  return data;
-}
-
-void sendString(char* data) {
-  while (*data != '\0') sendChar(*data++);
-}
-
-ISR(USART_RX_vect) {
-  selection = UDR0;
 }
